390dc32ea3c2857f6dec8eb79d5ad46274c7cd12
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-11-23 15:55:58 +0100

    fix: texture x offset

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/12 01:11:02 by ivork         #+#    #+#                 */
-/*   Updated: 2022/11/17 17:34:31 by ivork         ########   odam.nl         */
+/*   Updated: 2022/11/23 14:30:57 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,6 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-int textures[32][32]=
-{
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-};
-
 
 void	my_mlx_pixel_put(t_win *data, int x, int y, int color)
 {
@@ -84,10 +47,14 @@ int	get_color(mlx_texture_t* texture, int x, int y)
 	int red;
 	int green;
 	int blue;
-	
-	blue = texture->pixels[x * texture->width * 4+ (y * 4) + 1];
-	green = texture->pixels[x * texture->width * 4 + (y * 4) + 2];
-	red = texture->pixels[x * texture->width *4 + (y * 4) + 3];
+
+	// blue = texture->pixels[x * texture->width * 4 + (y * 4) + 1];
+	// green = texture->pixels[x * texture->width * 4 + (y * 4) + 2];
+	// red = texture->pixels[x * texture->width * 4 + (y * 4) + 3];
+
+	blue = texture->pixels[y * texture->width * 4 + (x * 4) + 1];
+	green = texture->pixels[y * texture->width * 4 + (x * 4) + 2];
+	red = texture->pixels[y * texture->width * 4 + (x * 4) + 3];
 	color = (256 * 256 * 256 * 256) + (blue * 256 * 256) + (green * 256) + red;
 	return (color);
 }
@@ -109,30 +76,32 @@ void build_wall(t_settings *data, double ray_length, int x, int color, double an
 	ty_off = 0;
     if (data->hitX == -1)
     {
-        tx = (double)((int)data->hitY % (screenHeight / mapHeight)) / (screenHeight / mapHeight) * (data->texture->width / 4.0);
+        // tx = (double)((int)data->hitY % (screenHeight / mapHeight)) / (screenHeight / mapHeight) * (data->texture->width / 4.0);
+        tx = (double)(fmod(data->hitY, ((double)screenHeight / (double)mapHeight))) / (screenHeight / mapHeight) * (data->texture->width);
 		if (angle > PI && angle < 2 * PI)
-			tx = (data->texture->width / 4) - tx;
+			tx = (data->texture->width) - tx;
     }
     else
 	{
-        tx = (float)((int)data->hitX % (screenHeight / mapHeight)) / (screenHeight / mapHeight) * (data->texture->width / 4.0);
+        // tx = (float)((int)data->hitX % (screenHeight / mapHeight)) / (screenHeight / mapHeight) * (data->texture->width / 4.0);
+        tx = (float)(fmod(data->hitX, (screenHeight / mapHeight))) / (screenHeight / mapHeight) * (data->texture->width);
 		if (angle < 0.5 * PI || angle > 1.5 * PI)
-			tx = (data->texture->width / 4) - tx;
+			tx = data->texture->width - tx;
 		
 	}
-	if (line_heigth > 624)
+	if (line_heigth > screenHeight)
 	{
-		ty_off = line_heigth - screenHeight / 2;
-		line_heigth = 624;
+		ty_off = (line_heigth - screenHeight) / 2.0;
+		line_heigth = screenHeight;
 	}
 	ty = (ty_off * texture_step_y);
 	i = 0;
 	while (i < screenHeight)
 	{
-		if (screenHeight - i <  (screenHeight + line_heigth) / 2)
+		if (screenHeight - i <=  (screenHeight + line_heigth) / 2)
 		{
 			j = 0;
-			while (j < line_heigth)
+			while (j <= line_heigth)
 			{
 				color2 = get_color(data->texture, tx, ty);
 				mlx_put_pixel(data->img, x,  i + j, color2);
