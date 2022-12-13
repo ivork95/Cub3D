4ce7795ddcd040135e044d10cc6cfe4ba7fb49a9
Idan Vork <70711888+ivork95@users.noreply.github.com>
2022-12-13 19:07:11 +0100

    fix: wall collision A-key

--- src/graphics/key_hook.c
+++ src/graphics/key_hook.c
@@ -6,7 +6,7 @@
 /*   By: ivork <ivork@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/12 00:55:31 by ivork         #+#    #+#                 */
-/*   Updated: 2022/12/13 16:06:22 by ivork         ########   odam.nl         */
+/*   Updated: 2022/12/13 18:54:38 by ivork         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,7 +94,7 @@ void	move_left_right(t_player *player, mlx_t *mlx, char **map)
 			[(int)(player->pos_x - player->plane_x * player->move_speed)]
 			== '0')
 			player->pos_x -= player->plane_x * player->move_speed;
-		if (map[(int)(player->pos_y + player->plane_y * player->move_speed)]
+		if (map[(int)(player->pos_y - player->plane_y * player->move_speed)]
 			[(int)player->pos_x] == '0')
 			player->pos_y -= player->plane_y * player->move_speed;
 	}
